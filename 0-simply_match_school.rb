a469699e5b6fdca0c4a80fa89c63f4bbbe2d81d2
clencyc <clency2023@gmail.com>
2024-03-03 19:05:08 +0300

    Removed unnecessary files

--- 0-simply_match_school.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-#  Ruby script that accepts one argument and pass it to a regular expression matching method
-#  The regular expression must match school
-
-puts ARGV[0].scan(/Scho.l/).join
--- 6-phone_number.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-#10 digin phone number
-
-puts ARGV[0].scan(/^\d{10}$/).join
