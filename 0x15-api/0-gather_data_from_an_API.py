29a44ba1f9481f781469fbefd42658dce57fa314
clencyc <clency2023@gmail.com>
2024-05-21 23:38:11 -0700

    getting a url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 
-"""Gathering data form an API"""
+"""Gathering data from an API"""
 
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    url = f'https://jsonplaceholder.typicode.co/todos?userId={employee_id}'
-
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Error":, response.text)
-        return
-
-        todo_list = response.json()
-        total_tasks = len(todo_list)
-        completed_tasks = sum(1 for task in todo_list if task['completed'])
-        employee_name = todo_list[0]['name']
-
-         print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
-         for task in todo_list:
-             if task['completed']:
-                 print(f"\t{task['title']}")
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    params = {"userId":employee_id}
 
-        employee_id = int(sys.argv[1])
+    todos_response = requests.get(url + "/todos", params=params)
+    todos = todos_response.json()
+    completed = []
+    for todo in todos:
+        if todo.get("competed"):
+            completed.append(todo.get("title"))
 
-        get_employee_todo_progress(employee_id)
+    print("Employee {} is done with tasks({}/{})".format(user.get("name"), len(completed), len(todo)))
+
+    for complete in completed:
+        print("t {}".format(complete))
