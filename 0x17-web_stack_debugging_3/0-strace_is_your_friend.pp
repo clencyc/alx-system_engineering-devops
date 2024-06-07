b642d9129a49a4d8eaf118b2cff550f8a8181cec
clencyc <clency2023@gmail.com>
2024-06-07 08:49:03 +0300

    corrected

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
-#  check why apache returns 500 error and fix it
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-file { '/var/www/html/wp-settings.php':
-    ensure => present,
-    owner => 'www-data',
-    group => 'www-data',
-    mode => '0644',
-}
-
-exec {
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path            => '/usr/local/bin/:/bin/'
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
