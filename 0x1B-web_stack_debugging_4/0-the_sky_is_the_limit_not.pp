0d6f4b129ef3e67e78e2f480df6dc2ec764a7c06
clencyc <clency2023@gmail.com>
2024-07-15 12:30:28 +0300

    task 1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# will increase the user limit to 4096
+
+exec { 'fixnginx':
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path            => '/usr/local/bin/:/bin',
+}
+
+# Restart the server
+
+exec { 'nginx_restart':
+    command => 'nginx restart',
+    path            => '/etc/init.d/'
+
+}
